218b9d758c0e037b19b0cf0658bc2bba85cfb26d
Sai Santhosh Chanda <saisanthoshchanda1982@gmail.com>
2024-03-20 04:26:36 -0500

    utils added

---
+++ demo.py
@@ -0,0 +1,11 @@
+from us_visa.logger import logging
+
+logging.info("We are using Curtom Log")
+
+# from us_visa.logger import logging
+# from us_visa.exception import USvisaException
+# import sys
+# try:
+#     a = 1 / '10'
+# except Exception as e:
+#     raise USvisaException(e,sys) from e
