57f75a56c815eee9041437a95a4f6ad2dcfa4b76
Sai Santhosh Chanda <saisanthoshchanda1982@gmail.com>
2024-03-23 18:50:28 -0500

    Data ingestion added

--- demo.py
+++ demo.py
@@ -1,8 +1,14 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
-try:
-    a = 1 / '10'
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e,sys) from e
+# from us_visa.logger import logging
+# from us_visa.exception import USvisaException
+# import sys
+# try:
+#     a = 1 / '10'
+# except Exception as e:
+#     logging.info(e)
+#     raise USvisaException(e,sys) from e
+
+from us_visa.pipeline.train_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
+
