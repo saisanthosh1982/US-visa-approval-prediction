a65221463c0f94c164080dc13ef633f045c1d704
Sai Santhosh Chanda <saisanthoshchanda1982@gmail.com>
2024-03-25 03:09:11 -0500

    data validation added

--- us_visa/entity/artifact_entity.py
+++ us_visa/entity/artifact_entity.py
@@ -5,3 +5,9 @@ from dataclasses import dataclass
 class DataIngestionArtifact:
     trained_file_path:str 
     test_file_path:str 
+
+@dataclass
+class DataValidationArtifact:
+    validation_status:bool
+    message: str
+    drift_report_file_path: str
